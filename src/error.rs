08da8928c0967bdef76838cfde3361f31fd73160
Hannes De Valkeneer <2261239+hdevalke@users.noreply.github.com>
2020-12-04 23:28:45 +0100

    refactor: use `thiserror`

--- src/error.rs
+++ src/error.rs
@@ -1,90 +1,26 @@
 use crate::conventional;
 use handlebars::{RenderError, TemplateError, TemplateFileError};
-use std::{fmt, io};
+use std::io;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub(crate) enum Error {
-    Git(git2::Error),
-    Io(io::Error),
-    Template(TemplateError),
-    TemplateFile(TemplateFileError),
-    Parser(conventional::ParseError),
-    Render(RenderError),
-    Url(url::ParseError),
+    #[error(transparent)]
+    Git(#[from] git2::Error),
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    #[error(transparent)]
+    Template(#[from] TemplateError),
+    #[error(transparent)]
+    TemplateFile(#[from] TemplateFileError),
+    #[error(transparent)]
+    Parser(#[from] conventional::ParseError),
+    #[error(transparent)]
+    Render(#[from] RenderError),
+    #[error(transparent)]
+    Url(#[from] url::ParseError),
+    #[error("check error")]
     Check,
+    #[error("canceled by user")]
     CancelledByUser,
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {
-            Self::Git(ref e) => write!(f, "{}", e),
-            Self::Io(ref e) => write!(f, "{}", e),
-            Self::Template(ref e) => write!(f, "{}", e),
-            Self::TemplateFile(ref e) => write!(f, "{}", e),
-            Self::Parser(ref e) => write!(f, "{}", e),
-            Self::Render(ref e) => write!(f, "{}", e),
-            Self::Url(ref e) => write!(f, "{}", e),
-            Self::Check => write!(f, "check error"),
-            Self::CancelledByUser => write!(f, "canceled by user"),
-        }
-    }
-}
-
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match *self {
-            Self::Git(ref e) => Some(e),
-            Self::Io(ref e) => Some(e),
-            Self::Template(ref e) => Some(e),
-            Self::TemplateFile(ref e) => Some(e),
-            Self::Parser(ref e) => Some(e),
-            Self::Render(ref e) => Some(e),
-            Self::Url(ref e) => Some(e),
-            Self::Check => None,
-            Self::CancelledByUser => None,
-        }
-    }
-}
-
-impl From<git2::Error> for Error {
-    fn from(e: git2::Error) -> Self {
-        Self::Git(e)
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(e: io::Error) -> Self {
-        Self::Io(e)
-    }
-}
-
-impl From<TemplateError> for Error {
-    fn from(e: TemplateError) -> Self {
-        Self::Template(e)
-    }
-}
-
-impl From<TemplateFileError> for Error {
-    fn from(e: TemplateFileError) -> Self {
-        Self::TemplateFile(e)
-    }
-}
-
-impl From<RenderError> for Error {
-    fn from(e: RenderError) -> Self {
-        Self::Render(e)
-    }
-}
-
-impl From<url::ParseError> for Error {
-    fn from(e: url::ParseError) -> Self {
-        Self::Url(e)
-    }
-}
-
-impl From<conventional::ParseError> for Error {
-    fn from(e: conventional::ParseError) -> Self {
-        Self::Parser(e)
-    }
 }
