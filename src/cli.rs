207a88c916a58753faa7f82a6072be39e85a8db6
Hannes De Valkeneer <2261239+hdevalke@users.noreply.github.com>
2023-12-08 21:14:09 +0100

    docs: explain the paths arguments are relative to the root of the repository
    Refs: #160

--- src/cli.rs
+++ src/cli.rs
@@ -66,6 +66,7 @@ pub struct VersionCommand {
     #[clap(long, requires = "bump", default_value_t = Prerelease::new("").unwrap())]
     pub prerelease: Prerelease,
     /// Only commits that update those <paths> will be taken into account. It is useful to support monorepos.
+    /// Each path should be relative to the root of the repository.
     #[clap(short = 'P', long, env = "CONVCO_PATHS")]
     pub paths: Vec<PathBuf>,
     /// Print the commit-sha of the version instead of the semantic version
@@ -130,6 +131,7 @@ pub struct ChangelogCommand {
     #[clap(long, env = "CONVCO_INCLUDE_HIDDEN_SECTIONS")]
     pub include_hidden_sections: bool,
     /// Only commits that update those <paths> will be taken into account. It is useful to support monorepos.
+    /// Each path should be relative to the root of the repository.
     #[clap(short = 'P', long, env = "CONVCO_PATHS")]
     pub paths: Vec<PathBuf>,
     /// Follow only the first parent of merge commits. Commits from the merged branche(s) will be discarded.
